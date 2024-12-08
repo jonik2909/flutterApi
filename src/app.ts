086aa64c17074884458aefe3e18f705d44167a2a
jonik2909 <boronovjonibek@gmail.com>
2024-12-08 10:26:41 +0900

    BRR: initialize setup

---
+++ src/app.ts
@@ -0,0 +1,54 @@
+import cors from "cors";
+import express from "express";
+import path from "path";
+import router from "./router";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import { MORGAN_FORMAT } from "./libs/config";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
+
+/** 1-ENTRANCE **/
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static("./uploads"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+app.use(cookieParser());
+app.use(morgan(MORGAN_FORMAT));
+
+/** 2-SESSIONS **/
+
+/** 3-VIEWS **/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/** 4-ROUTERS **/
+app.use("/", router);
+
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
+
+let summaryClient = 0;
+io.on("connection", (socket) => {
+  summaryClient++;
+  console.log(`Connection & total [${summaryClient}]`);
+
+  socket.on("disconnect", () => {
+    summaryClient--;
+    console.log(`Disconnection & total [${summaryClient}]`);
+  });
+});
+
+export default server;
