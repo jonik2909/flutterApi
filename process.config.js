9b78fc125a11f5721ee32e360352bc3ed115e1b6
jonik2909 <boronovjonibek@gmail.com>
2025-01-20 17:16:13 +0900

    fix: modify prod name

--- process.config.js
+++ process.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  apps : [{
-    name   : "BURAK",
-    cwd: "./",
-    script : "./dist/server.js",
-    watch: false,
-    env_production: {
-       NODE_ENV: "production"
+  apps: [
+    {
+      name: "FLUTTER-API",
+      cwd: "./",
+      script: "./dist/server.js",
+      watch: false,
+      env_production: {
+        NODE_ENV: "production",
+      },
+      env_development: {
+        NODE_ENV: "development",
+      },
+      instances: 1,
+      exec_mode: "cluster",
     },
-    env_development: {
-       NODE_ENV: "development"
-    },
-    instances: 1,
-    exec_mode: "cluster"
-  }]
-}
+  ],
+};
